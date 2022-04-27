f9a76711404bbea878995b65fb4eb0a141cda359
JKun <102599182+juhakun@users.noreply.github.com>
2022-04-27 12:04:02 +0200

    Buttons working

--- vue-website-jhk/src/components/TheProjects.vue
+++ vue-website-jhk/src/components/TheProjects.vue
@@ -26,7 +26,7 @@ export default {
         },
         {
           id: "02",
-          category: "de-exhibit",
+          category: "de-software",
           name: "Spot on Materials",
           description:
             "Die Ausstellung SPOT ON MATERIALS präsentierte innovative Materialen und veranschaulichte diese anhand bereits umgesetzter Produkte. Die Produkte und Materialien verdeutlichen, welchen Stellenwert Materialien als Generator für Kreativität, für neue Ideen und ungewöhnliche Lösungen haben. ",
@@ -44,12 +44,23 @@ export default {
           descriptionEng: "",
           img: require("@/assets/img/ECO_Darmstadt_01.jpg"),
         },
+
+        {
+          id: "",
+          category: "de-home",
+          name: "Hallo",
+          description:
+            "Die Ausstellung WOHER KOMMT DAS NEUE – KREATIVITÄT UND EINFALLSREICHTUM IM DESIGN stellte nicht das fertige Produkte, sondern den kreativen Prozess in den Mittelpunkt: Sensibilität, Leidenschaft, Aufgeschlossenheit für Neues und Unerwartetes und der Mut über Grenzen zu gehen prägen diesen Prozess und die Arbeit der Kreativen und ermöglichen die Entwicklung einzigartiger Ideen. ",
+          nameEng: "Where does the New come from?",
+          descriptionEng: "",
+          img: require("@/assets/img/ECO_Darmstadt_01.jpg"),
+        },
       ],
       filteredProjects: [
         {
-          id: "03",
-          category: "de-exhibit",
-          name: "Woher kommt das Neue?",
+          id: "",
+          category: "de-home",
+          name: "Hallo",
           description:
             "Die Ausstellung WOHER KOMMT DAS NEUE – KREATIVITÄT UND EINFALLSREICHTUM IM DESIGN stellte nicht das fertige Produkte, sondern den kreativen Prozess in den Mittelpunkt: Sensibilität, Leidenschaft, Aufgeschlossenheit für Neues und Unerwartetes und der Mut über Grenzen zu gehen prägen diesen Prozess und die Arbeit der Kreativen und ermöglichen die Entwicklung einzigartiger Ideen. ",
           nameEng: "Where does the New come from?",
@@ -63,7 +74,7 @@ export default {
   methods: {
     filterProjects(cat) {
       this.filteredProjects = this.projects.filter(
-        (proj) => proj.category !== cat
+        (proj) => proj.category === cat
       );
       console.log(this.filteredProjects);
     },
